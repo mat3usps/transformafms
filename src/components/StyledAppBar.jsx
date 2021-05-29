050ada45c17c67775a5d06eb77bb64be4cbcc2f7
Mateus Pereira <mateusp.s@outlook.com>
2021-05-28 22:44:17 -0300

    finishing touches

--- src/components/StyledAppBar.jsx
+++ src/components/StyledAppBar.jsx
@@ -10,7 +10,7 @@ import StyledBox from "../components/StyledDiv";
 const PixAppBar = styled(AppBar)({
   background: "linear-gradient(45deg, #494843 30%, #FF2929 90%)",
   color: "#000000",
-  height: 65,
+  height: 58,
 });
 
 const StyledAppBar = () => {
