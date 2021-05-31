5689c4bd76150748f144b8b1de164f448a0d4f72
Mateus Pereira <mateusp.s@outlook.com>
2021-05-30 21:05:46 -0300

    added: responsiviness to appBarButtons

--- src/components/AppBarMenus.jsx
+++ src/components/AppBarMenus.jsx
@@ -2,9 +2,14 @@ import { Button, Typography } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link, useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
 const AppBarMenus = () => {
   const [showMenus, setShowMenus] = useState(false);
+
+  const theme = useTheme();
+  const smallDisplay = useMediaQuery(theme.breakpoints.up("sm"));
 
   const location = useLocation();
   const { pathname } = location;
@@ -31,31 +36,70 @@ const AppBarMenus = () => {
         justifyContent: "center",
       }}
     >
-      <Button onClick={didShowMenu}>
-        <MenuIcon style={{ color: "#FFFFFF", fontSize: 40 }} />
-      </Button>
-      {showMenus && (
-        <div
-          style={{
-            backgroundColor: "white",
-            position: "absolute",
-            top: "58px",
-            marginLeft: "-90px",
-            border: "2px solid #FF2929",
-            borderRadius: "5px",
-            width: "150px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <Button>
-            <Link to="/" style={{ textDecoration: "none" }}>
+      {!smallDisplay ? (
+        <div>
+          <Button onClick={didShowMenu}>
+            <MenuIcon style={{ color: "#FFFFFF", fontSize: 40 }} />
+          </Button>
+          {showMenus && (
+            <div
+              style={{
+                backgroundColor: "white",
+                position: "absolute",
+                top: "58px",
+                marginLeft: "-90px",
+                border: "2px solid #FF2929",
+                borderRadius: "5px",
+                width: "150px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Button>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  <Typography>Início</Typography>
+                </Link>
+              </Button>
+              <Button>
+                <Link to="/contato" style={{ textDecoration: "none" }}>
+                  <Typography>Contato</Typography>
+                </Link>
+              </Button>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          <Button
+            style={{
+              height: "70px",
+            }}
+          >
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "#FFFFFF",
+                fontSize: "40px",
+              }}
+            >
               <Typography>Início</Typography>
             </Link>
           </Button>
-          <Button>
-            <Link to="/contato" style={{ textDecoration: "none" }}>
+          <Button
+            style={{
+              height: "70px",
+            }}
+          >
+            <Link
+              to="/contato"
+              style={{
+                textDecoration: "none",
+                color: "#FFFFFF",
+                fontSize: "40px",
+              }}
+            >
               <Typography>Contato</Typography>
             </Link>
           </Button>
